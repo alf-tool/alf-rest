84da93c8af076ceadf15819e799de6338cd8bdef
Bernard Lambeau <blambeau@gmail.com>
2012-10-11 10:03:21 +0200

    Enhance Helpers with database-related methods.

--- lib/alf/rest/helpers.rb
+++ lib/alf/rest/helpers.rb
@@ -1,6 +1,7 @@
 module Alf
   module Rest
     module Helpers
+      extend Forwardable
       include Payload::Server
 
       def alf_config
@@ -8,9 +9,24 @@ module Alf
       end
 
       def alf_agent
-        @alf_agent ||= Agent.new(self, alf_config)
+        @alf_agent ||= alf_config.agent_class.new(self, alf_config)
       end
       alias_method :agent, :alf_agent
+
+      def db_conn
+        alf_config.connection
+      end
+
+      def with_db_conn
+        yield(db_conn)
+      end
+
+      def_delegators :db_conn, :relvar,
+                               :query,
+                               :tuple_extract,
+                               :assert!,
+                               :fact!,
+                               :deny!
 
     end
   end
