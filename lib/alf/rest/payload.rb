bd3a1221ed4b0841419048838821ef939044f590
Bernard Lambeau <blambeau@gmail.com>
2013-02-06 18:51:46 +0100

    Add Payload#to_rack_response.

--- lib/alf/rest/payload.rb
+++ lib/alf/rest/payload.rb
@@ -14,6 +14,27 @@ module Alf
         to_relation(heading).tuple_extract
       end
 
+      def to_rack_response(env, status = 200)
+        mime_type = env['HTTP_ACCEPT'] || 'application/json'
+        [ status, {'Content-Type' => mime_type}, dump(env, mime_type) ]
+      end
+
+    private
+
+      def dump(env, mime_type)
+        if RelationLike===raw
+          Alf::Renderer.by_mime_type(mime_type, raw).each
+        else
+          case env['HTTP_ACCEPT']
+          when NilClass, /json/ then [ raw.to_json ]
+          when /yaml/           then [ raw.to_yaml ]
+          when /text\/plain/    then [ raw.to_s    ]
+          else
+            raise UnsupportedMimeTypeError, "Unsupported MIME type `#{mime_type}`"
+          end
+        end
+      end
+
     end # class Payload
   end # module Rest
 end # module Alf
